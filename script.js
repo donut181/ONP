4845ad639744db254fc05aa43e15b3e24bea11bb
Tadeusz Polok <tada@TP-Lap>
2016-07-14 21:55:35 +0200

    komentarzu nie trza

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function sendQuery(){
 	var log = document.getElementById("log");
 	log.innerHTML += "Yo!<br />";
 }
-function trololol(){
+function tr33lol(){
 	var log = document.getElementById("log");
 	log.innerHTML += "trololo<br />";
+}
+function trololo2l(){
+	var log = document.getElementById("log");
+	  log.innerHTML += "Niy!<br />";
 }
