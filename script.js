4d5c2903fc5445b6680862c379060cd5a32d63f3
mpolok <mpro12@gmail.com>
2016-07-14 21:48:26 +0200

    troloo

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function sendQuery(){
 	var log = document.getElementById("log");
 	log.innerHTML += "Yo!<br />";
 }
+function trololol(){
+	var log = document.getElementById("log");
+	log.innerHTML += "trololo<br />";
+}
