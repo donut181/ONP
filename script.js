c9999a5cfac83a79a3d08dbcdc9e52a61fd6d8f7
Tadeusz Polok <tada@TP-Lap>
2016-07-18 22:28:37 +0200

    duzo dodane do skrypt.js. Przyszlym razem odnosimy sie do Wikipedii.

--- script.js
+++ script.js
@@ -8,7 +8,47 @@ function sendQuery(){
 function getMeWynik(str) 
 {
 	var strArray = str.split('');
-	var cos = new Array();
+	stackOperators = new Array();
+	strArray.forEach(myfunction);
+	while(stackOperators.length > 0){
+		document.getElementById("output").innerHTML += stackOperators.pop() + "<br />";
+	}
+
+}
+
+function myfunction(element, index, array)
+{
+	var output = document.getElementById("output");
+	var log = document.getElementById("log");
+	
+	if (checkIfOperator(element))
+	{
+		if(element==")")
+		{
+			log.innerHTML += "element jest nawiasem ) ! <br />"
+			while(stackOperators[stackOperators.length-1]!="(")     
+				output.innerHTML += stackOperators.pop();
+			stackOperators.pop();/*usuwanie "(" z stack*/
+		}
+		else
+		{
+			if(stackOperators.length>0)
+			{
+				if (priorytet(element)<=stackOperators[stackOperators.length-1]) 
+				{
+					log.innerHTML += "wszedlem do if element <= elementStackOperator <br />"
+					output.innerHTML += stackOperators.pop() + "<br />"	
+				}	
+			}	
+			log.innerHTML += "dodaje element " + element + "do stackOperators <br />"
+			stackOperators.push(element)
+		}
+	}
+	else
+		output.innerHTML += element;
+	if(element == array[array.length-1]){
+		log.innerHTML+= stackOperators.toString();
+	}
 }
 
 function checkIfOperator(zmienna)
@@ -30,16 +70,50 @@ function checkIfOperator(zmienna)
 		case "=":
 			return true;
 		    break;
-		case "s":
-			return "?";
-			break;
 		case "(":
 			return true;
 			break;
 		case ")":
 			return true;
 			break;
+		case "^":
+			return true;
+			break;
 		default:
+		    return false;
+	}	
+	
+}
+function priorytet(zmienna)
+{
+	switch (zmienna)
+	{	
+		case "+":
+			return 0; 
+			break; 
+		case "-":
+			return 0;
+		    break;
+		case "*":
+			return 1;
+		    break;
+		case "/":
+			return 1;
+		    break;
+		case "=":
+			return "?";
+		    break;
+		case "(":
+			return 10;
+			break;
+		case ")":
+			return 10;
+			break;
+		case "^":
+			return 2;
+			break;
+		default:
+			document.getElementById("log").innerHTML += "Nieznany operator(funckja priorytet)";
 		    return false;
 	}	
 	
