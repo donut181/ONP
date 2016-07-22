37c04576f98a5e512c8acdf9a05b8ee7b842a2f6
Tadeusz Polok <tada@TP-Lap>
2016-07-22 14:01:40 +0200

    wreszcie dziala zamiana notacji infiksowej na ONP. Nastepnym razem trzeba ogarnac wieksze liczby i inne operatory

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function sendQuery(){
+	document.getElementById("output").innerHTML = "";
 	var log = document.getElementById("log");
 	var str = document.getElementById("input").value.replace(/\s+/g, '');
-	log.innerHTML += str + "<br/>";
+	log.innerHTML = str + "<br/>";
 	var wynik = getMeWynik(str);
 }
 
@@ -11,73 +12,61 @@ function getMeWynik(str)
 	stackOperators = new Array();
 	strArray.forEach(myfunction);
 	while(stackOperators.length > 0){
-		document.getElementById("output").innerHTML += stackOperators.pop() + "<br />";
+		outputAdd(stackOperators.pop());
 	}
 
 }
-
+function outputAdd(str)
+{
+	document.getElementById("output").innerHTML += str+";";
+}
 function myfunction(element, index, array)
 {
-	var output = document.getElementById("output");
 	var log = document.getElementById("log");
-	
-	if (checkIfOperator(element))
+	if (element=="(")
+		stackOperators.push(element);
+	if(element==")")
 	{
-		if(element==")")
-		{
-			log.innerHTML += "element jest nawiasem ) ! <br />"
-			while(stackOperators[stackOperators.length-1]!="(")     
-				output.innerHTML += stackOperators.pop();
-			stackOperators.pop();/*usuwanie "(" z stack*/
-		}
-		else
-		{
-			if(stackOperators.length>0)
-			{
-				if (priorytet(element)<=stackOperators[stackOperators.length-1]) 
-				{
-					log.innerHTML += "wszedlem do if element <= elementStackOperator <br />"
-					output.innerHTML += stackOperators.pop() + "<br />"	
-				}	
-			}	
-			log.innerHTML += "dodaje element " + element + "do stackOperators <br />"
-			stackOperators.push(element)
-		}
+		log.innerHTML += "element jest nawiasem ) ! <br />"
+		while(stackOperators[stackOperators.length-1]!="(")     
+			outputAdd(stackOperators.pop());
+		stackOperators.pop();/*usuwanie "(" z stack*/
 	}
-	else
-		output.innerHTML += element;
-	if(element == array[array.length-1]){
+	if (checkIfOperator(element).operator)	/*dopoki na stosie jest jakis operator i:*/
+	{
+		while(stackOperators.length>0 && ((checkIfOperator(element).lacznosc=="left"&&priorytet(element)<=priorytet(stackOperators[stackOperators.length-1])) || (checkIfOperator(element).lacznosc=="right"&&priorytet(element)<priorytet(stackOperators[stackOperators.length-1])) ))
+		{
+			outputAdd(stackOperators.pop());
+		}		
+		log.innerHTML += "dodaje element " + element + "do stackOperators <br />";
+		stackOperators.push(element);
+	}
+	else if (element!="("&&element!=")")
+		outputAdd(element);
+	/*Jeśli dojdziemy do końca wyrażenia, to ze stosu operatorów pobieramy operatory 
+	i przenosimy je kolejno na wyjście aż do wyczyszczenia stosu. Algorytm kończymy.*/
+	if(index == array.length-1){
 		log.innerHTML+= stackOperators.toString();
 	}
 }
-
 function checkIfOperator(zmienna)
 {
 	switch (zmienna)
 	{	
 		case "+":
-			return true; 
+			return {operator:true, lacznosc:"left"}; 
 			break; 
 		case "-":
-			return true;
+			return {operator:true, lacznosc:"left"};
 		    break;
 		case "*":
-			return true;
+			return {operator:true, lacznosc:"left"};
 		    break;
 		case "/":
-			return true;
+			return {operator:true, lacznosc:"left"};
 		    break;
-		case "=":
-			return true;
-		    break;
-		case "(":
-			return true;
-			break;
-		case ")":
-			return true;
-			break;
 		case "^":
-			return true;
+			return {operator:true, lacznosc:"right"};
 			break;
 		default:
 		    return false;
@@ -88,29 +77,26 @@ function priorytet(zmienna)
 {
 	switch (zmienna)
 	{	
-		case "+":
+		case "(":
 			return 0; 
 			break; 
-		case "-":
+		case ")":
 			return 0;
 		    break;
-		case "*":
+		case "+":
+			return 1; 
+			break; 
+		case "-":
 			return 1;
+		    break;
+		case "*":
+			return 2;
 		    break;
 		case "/":
-			return 1;
-		    break;
-		case "=":
-			return "?";
-		    break;
-		case "(":
-			return 10;
-			break;
-		case ")":
-			return 10;
-			break;
-		case "^":
 			return 2;
+		    break;
+		case "^":
+			return 3;
 			break;
 		default:
 			document.getElementById("log").innerHTML += "Nieznany operator(funckja priorytet)";
